59a364381d8d9439b6ec4bb3548559f5c3d9ee41
Avesheak <fahimshirz223@gmail.com>
2025-05-12 14:36:14 +0600

    New

---
+++ Day 1/Project.py
@@ -0,0 +1,9 @@
+list = ["apple", "Home", "numpy"]
+print (len(list))
+if "null" in list:
+ print ("yes apple in the list")
+else:
+  print ("no apple in the list")
+list[1] = "bannana"
+print (list)
+New updateS how 
