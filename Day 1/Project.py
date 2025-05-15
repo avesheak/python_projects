084e71ceeee62495ea47f966f1393640b28a0728
Avesheak <fahimshirz223@gmail.com>
2025-05-15 09:00:50 +0600

    String adding in code

--- Day 1/Project.py
+++
@@ -1,9 +0,0 @@
-list = ["apple", "Home", "numpy"]
-print (len(list))
-if "null" in list:
- print ("yes apple in the list")
-else:
-  print ("no apple in the list")
-list[1] = "bannana"
-print (list)
-New updateS how 
---
+++ Working with Variables in Python to Manage Data/Task 1.py
@@ -0,0 +1,5 @@
+##Using line brake into the code
+print ("Hello World\nWhat is the name of the day")
+
+## Adding two lines into the strings. 
+print ("Hello World" +" " + "What is the name of the day")
