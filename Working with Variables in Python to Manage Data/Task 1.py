4e2453b6bf9e067c10d193a012bc99372cb85841
Avesheak <fahimshirz223@gmail.com>
2025-05-15 16:49:49 +0600

    Day 1 Project Name Generator

--- Working with Variables in Python to Manage Data/Task 1.py
+++ Working with Variables in Python to Manage Data/Task 1.py
@@ -3,3 +3,21 @@ print ("Hello World\nWhat is the name of the day")
 
 ## Adding two lines into the strings. 
 print ("Hello World" +" " + "What is the name of the day")
+
+## Taking inpute 
+print ( "Hello " + input  ("what is your name ") + " !")
+
+## taking the number from the input
+Name = input("What is your name ")
+Lenth = len(Name)
+print (Lenth)
+
+## Sample two
+print (len(input("What is your name")))
+
+## Brand Name genarator 
+City = input("Which city you live in ")
+print (City)
+Name = input("What is your Name ") 
+print (Name)
+print ("Your Brand Name is " + City + " " + Name )
