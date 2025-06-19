5871ebe5fc49dc0325f3e9b91740b692eb60d274
Avesheak <chrystalvargas250@gmail.com>
2025-06-19 16:13:31 +0600

    Task 6 Complete

--- Python Functions & Karel/Task 6.py
+++ Python Functions & Karel/Task 6.py
@@ -4,6 +4,7 @@ myfunction()
 
 ## This code is only run on https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 ## It will not work here.
+
 def movement():
     move()
     turn_left()
@@ -53,4 +54,23 @@ while at_goal() == False:
         turn_left()
         move()
         turn_left()
-        
+
+## Random Blocks and End points 
+
+while at_goal() == False:
+    if front_is_clear() == True:
+        move()
+    if wall_in_front() == True:
+        turn_left()
+    elif wall_on_right() == True:
+            move()
+    if right_is_clear() == True:
+        turn_left()
+        turn_left()
+        turn_left()
+        move()
+        turn_left()
+        turn_left()
+        turn_left()
+    if object_here() == True:
+        turn_left()
