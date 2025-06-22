65e34d97d923440268a3b29e08dd62ff23aaf938
Avesheak <chrystalvargas250@gmail.com>
2025-06-22 16:43:20 +0600

    Unsolved

--- Hangman/Task 7.py
+++ Hangman/Task 7.py
@@ -1,6 +1,16 @@
 import random 
 word_list = ["aardvark", "baboon", "camel"]
-choice_word = random.choice(word_list)
-user_word = input("Put a word ")
+choice_word = random.choice(word_list).lower()
+guess = input("put a letter ")
+display = []
+for _ in choice_word:
+    display += "_"
+print (display)
+for possition in range(len(choice_word)):
+    letter = choice_word[possition]
+    if letter == guess:
+        display[possition] = letter
+    else:
+        print("Wrong") 
 
 
