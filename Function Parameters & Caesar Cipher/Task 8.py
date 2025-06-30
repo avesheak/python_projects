08b72363bd3b0510c6703a41f0d4009d064f6ee7
Avesheak <chrystalvargas250@gmail.com>
2025-06-30 16:53:44 +0600

    cipher code

--- Function Parameters & Caesar Cipher/Task 8.py
+++ Function Parameters & Caesar Cipher/Task 8.py
@@ -49,30 +49,51 @@ pnum(number)
 
 ## Chiper Text
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-chiper_text = ""
-
 def encrypt(text,shift):
-    for letter in chiper_text:
+    chiper_text = ""
+    for letter in text:
         possiction = alphabet.index(letter)
+        new_possition = possiction + shift
+        decript = alphabet[new_possition]
+        chiper_text += decript
+    print (f"Your encoded text is {chiper_text}")
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+def deencrypt (text,shift):
+    chiper_text = ""
+    for letter in text:
+        possiction = alphabet.index(letter)
+        new_possition = possiction - shift
+        decript = alphabet[new_possition]
+        chiper_text += decript
+    print (f"Your decode text is {chiper_text}")
+if direction == "encode":
+    encrypt(text,shift)
+elif direction == "decode":
+    deencrypt(text,shift)
+else:
+    print("You put a wrong input")
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+## Optimizing the cipher code
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+def small (direction,text,shift):
+    cipher_text= ""
+    if direction == "encode":
+        encrypt(text,shift)
+    elif direction == "decode":
+        deencrypt(text,shift)
+    else:
+        print("You put a wrong input")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
 
 
 
