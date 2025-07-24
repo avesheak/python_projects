f172ed55c1b0c6643c27553388179a625b984154
Avesheak <chrystalvargas250@gmail.com>
2025-07-24 16:02:06 +0600

    Day 10 task 1

---
+++ Functions with Outputs/task 10.py
@@ -0,0 +1,9 @@
+#Task 10.1 
+def formate_name(f_name,l_name):
+    a =f_name.title()
+    b =l_name.title()
+    output = (a,b)
+    return output
+print(formate_name("KarIme", "HassaIn"))
+
+# Task 10.2
