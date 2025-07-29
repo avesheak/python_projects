b7e6ff10ea5c72f07175afdca82711f08e7e35b7
avesheak <chrystalvargas250@gmail.com>
2025-07-29 22:25:31 +0600

    day 10.2

--- Functions with Outputs/task 10.py
+++ Functions with Outputs/task 10.py
@@ -6,4 +6,30 @@ def formate_name(f_name,l_name):
     return output
 print(formate_name("KarIme", "HassaIn"))
 
-# Task 10.2 new update
+# Task 10.2
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year,month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  if is_leap(year) is True and month ==2:
+    return 28
+  return month_days[month-1]
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+#Task 10.3 
